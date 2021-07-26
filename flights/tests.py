325c76bc74fe655aad0468f4a1261d5155ba6f9a
Gowtham Kumar Vulluri <61021745+gowtham-prog@users.noreply.github.com>
2021-07-26 22:38:59 +0530

    Update tests.py

--- flights/tests.py
+++ flights/tests.py
@@ -34,48 +34,4 @@ class FlightTestCase(TestCase):
         a2 = Airport.objects.get(Code="BBB")
         f = Flight.objects.get(Origin=a1, Destination=a2, Duration=-100)
         self.assertFalse(f.is_valid_flight())
-    def test_index(self):
-
-    # Set up client to make requests
-        c = Client()
-
-    # Send get request to index page and store response
-        response = c.get("/flights/")
-
-    # Make sure status code is 200
-        self.assertEqual(response.status_code, 200)
-
-    # Make sure three flights are returned in the context
-        self.assertEqual(response.context["flights"].count(), 3)
-    def test_valid_flight_page(self):
-        a1 = Airport.objects.get(Code="AAA")
-        f = Flight.objects.get(Origin=a1, Destination=a1)
-
-        c = Client()
-        response = c.get(f"/{f.id}/book")
-        self.assertEqual(response.status_code, 200)
-
-    def test_invalid_flight_page(self):
-        max_id = Flight.objects.all().aggregate(max("id"))["max_id"]
-
-        c = Client()
-        response = c.get(f"/{max_id + 1}")
-        self.assertEqual(response.status_code, 404)
-    def test_flight_page_passengers(self):
-        f = Flight.objects.get(pk=1)
-        p = Passenger.objects.create(first="Alice", last="Adams")
-        f.passengers.add(p)
-
-        c = Client()
-        response = c.get(f"/flights/{f.id}")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["passengers"].count(), 1)
-
-    def test_flight_page_non_passengers(self):
-        f = Flight.objects.get(pk=1)
-        p = Passenger.objects.create(first="Alice", last="Adams")
-
-        c = Client()
-        response = c.get(f"/flights/{f.id}")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.context["non_passengers"].count(), 1)
+   
